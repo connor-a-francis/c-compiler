9ee5409cf33184a7e749dda7e0672ee3b6a49ee2
Connor Francis <cfster03@gmail.com>
2025-09-18 12:59:35 -0400

    parser framework

--- include/compiler/parser.h
+++ include/compiler/parser.h
@@ -9,19 +9,33 @@ public:
     Parser(std::vector<Token> token_input){
         current = 0;
         std::vector<Token> tokens = token_input;
+        std::vector<Expr> statements;
         tokens.reserve(100000);
+        statements.reserve(100000);
     }
 
 private:
     int current;
+    std::vector<Expr> statements;
     std::vector<Token> tokens;
-    AST::Expr expression();
-    AST::Expr assign();
-    AST::Expr binary();
-    AST::Expr call();
-    AST::Expr grouping();
-    AST::Expr literal();
-    AST::Expr variable();
-    
-};
+    AST::Expr parseProgram();
+    AST::Expr parseExpression();
 
+    AST::Expr parseStatement();
+    AST::Expr parseDefinition();
+    AST::Expr parseBinding();
+    AST::Expr parseExtern();
+    AST::Expr parseReturn();
+    AST::Expr parseCall();
+    AST::Expr parseBinaryExpr();
+    AST::Expr parseGrouping();
+    AST::Expr parseRands();
+    AST::Expr parseRand();
+    AST::Expr parseOperator();
+    Token moveReadHead();
+    bool isAtEnd();
+    Token peek();
+    Token previous();
+    bool match(std::initializer_list<Token> types);
+    bool check(Token type);
+};
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,10 +1,64 @@
-#include "compiler/token.h"
-#include "compiler/ast.h"
 #include "compiler/parser.h"
+#include "compiler/ast.h"
+#include "compiler/token.h"
+#include <type_traits>
+#include <variant>
 #include <vector>
 
+AST::Expr Parser::parseProgram() { 
 
+  return Expr{};
 
-AST::Expr Parser::expression() {
-  // return equality();
 }
+AST::Expr Parser::parseExpression() { 
+  return Expr{};
+}
+
+
+// class Assign;
+// class Binary;
+// class Call;
+// class Grouping;
+// class Literal;
+// class Variable;
+//
+AST::Expr Parser::parseStatement() { return Expr{}; }
+AST::Expr Parser::parseDefinition() { return Expr{}; }
+AST::Expr Parser::parseBinding() { return Expr{}; }
+AST::Expr Parser::parseExtern() { return Expr{}; }
+AST::Expr Parser::parseReturn() { return Expr{}; }
+AST::Expr Parser::parseCall() { return Expr{}; }
+AST::Expr Parser::parseBinaryExpr() { return Expr{}; }
+AST::Expr Parser::parseGrouping() { return Expr{}; }
+AST::Expr Parser::parseRands() { return Expr{}; }
+AST::Expr Parser::parseRand() { return Expr{}; }
+AST::Expr Parser::parseOperator() { return Expr{}; }
+
+bool Parser::match(std::initializer_list<Token> types) {
+    for (auto type : types) {
+        if (check(type)) {
+            moveReadHead();
+            return true;
+        }
+    }
+    return false;
+}
+Token Parser::moveReadHead() {
+    if (!isAtEnd()) current++;
+    return previous();
+  }
+
+bool Parser::isAtEnd() {
+    return std::holds_alternative<EndOfFile>(peek());
+  }
+
+Token Parser::peek() {
+    return this->tokens[this->current];
+  }
+bool Parser::check(Token type) {
+    return std::is_same<decltype(peek()), decltype(type)>::value;
+  }
+
+Token Parser::previous() {
+    return this->tokens[this->current - 1];
+  }
