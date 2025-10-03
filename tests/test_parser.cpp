06839026398db3a5e659b81b1d74ef82f52d85fd
Connor Francis <cfster03@gmail.com>
2025-10-03 11:06:12 -0400

    first working parser test

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -1,55 +1,42 @@
-// #include "gtest/gtest.h"
-// #include <cassert>
-// #include <gtest/gtest.h>
-// #include "compiler/parser.h"
-// #include "compiler/token.h"
-// #include "compiler/ast.h"
-// #include "../src/ast_printer.cpp"
-// #include <vector>
-// 
-// typedef std::vector<Token> tokenStream;
-// typedef std::vector<std::shared_ptr<Expr>> program;
-// 
-// void test(tokenStream input, program expected);
-// 
-// TEST(ParserTest, ParserBuilds)
-// {
-//     Parser result({TokenType::END_OF_FILE});
-// }
-// TEST(ParserTest, ParserReadsDef)
-// {
-// // def hello() {}
-//   test({TokenType::DEF,
-//        {TokenType::IDENTIFIER, std::string("hello")},
-//         TokenType::L_PAREN,
-//         TokenType::R_PAREN,
-//         TokenType::L_BRACE,
-//         TokenType::R_BRACE,
-//         TokenType::END_OF_FILE},
-// 
-//        {std::make_shared<Definition>(Definition(std::make_shared<Token>(TokenType::IDENTIFIER, string("hello")), {}, {}))}
-//        );
-// }
-// 
-// 
-// TEST(ParserTest, ParserReadsExtern)
-// {
-//   // extern hello;" 
-// 
-//     // test({TokenType::EXTERN,
-//     //      {TokenType::IDENTIFIER, std::string("hello")},
-//     //       TokenType::EOL,
-//     //       TokenType::END_OF_FILE}
-//     //       {std::make_shared<Extern>(Extern())}
-//     //      );
-// }
-// 
-// void test(tokenStream input, program expected) {
-//   std::ostringstream input_string;
-//   std::ostringstream expected_string;
-//   AstPrinter(input_string).print(Parser(input).parseProgram()), AstPrinter(expected_string).print(expected);
-//   cout << expected_string.str() << std::endl;
-//   ASSERT_EQ(input_string.str(), expected_string.str());
-// }
-// 
-// 
+#include "gtest/gtest.h"
+#include <cassert>
+#include <gtest/gtest.h>
+#include "compiler/parser.h"
+#include "compiler/token.h"
+#include "compiler/ast.h"
+#include "../src/ast_printer.cpp"
+#include <vector>
+
+using enum TokenType;
+
+typedef std::vector<Token> tokenStream;
+typedef std::vector<std::shared_ptr<Expr>> program;
+
+void test(tokenStream input, program expected);
+
+TEST(ParserTest, ParserBuilds)
+{
+    Parser result({{END_OF_FILE, 0, 0, 1}});
+}
+TEST(ParserTest, ParserReadsDef)
+{
+// def hello() {}
+  test(
+    
+       {Token(DEF, 0, 0, 3), Token(IDENTIFIER, 0, 4, 5, std::string("hello")),
+        Token(L_PAREN, 0, 9, 1), Token(R_PAREN, 0, 10, 1),
+        Token(L_BRACE, 0, 12, 1), Token(R_BRACE, 0, 13, 1),
+        Token(END_OF_FILE, 0, 14, 1)},
+       {std::make_shared<Definition>(Definition(std::make_shared<Token>(TokenType::IDENTIFIER, 0, 4, 5, string("hello")), {}, {}))}
+       );
+}
+
+void test(tokenStream input, program expected) {
+  std::ostringstream input_string;
+  std::ostringstream expected_string;
+  AstPrinter(input_string).print(Parser(input).parseProgram()), AstPrinter(expected_string).print(expected);
+  cout << expected_string.str() << std::endl;
+  ASSERT_EQ(input_string.str(), expected_string.str());
+}
+
+
