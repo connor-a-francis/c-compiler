9b0582650a78ebcf73f9e5ac1e78159dc53dfc6f
Connor Francis <cfster03@gmail.com>
2025-10-07 13:07:40 -0400

    include tokens in parse_error (#7)

--- include/compiler/parse_error.h
+++ include/compiler/parse_error.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <stdexcept>
+#include "compiler/token.h"
 using namespace std;
 
 
 class parse_error : public runtime_error {
     public :
-       parse_error(const std::string& msg) : runtime_error(msg) {
-    }
+       parse_error(Token tok, const std::string& msg) : runtime_error(msg), tok(std::move(tok)) { }
+    private:
+        Token tok;
 };
 
